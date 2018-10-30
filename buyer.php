a919eb39e3cc177c1ad17a98015650263bf99957
SteveThomi <35294606+SteveThomi@users.noreply.github.com>
2018-10-30 10:42:56 +0300

    Add files via upload

--- buyer.php
+++ buyer.php
@@ -1,62 +1,122 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="veiwpoint" content="widt=device-width, initial-scale=1">
+<title>Search</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
+<style>
+.maindiv{
+	align: center;
+	margin-top: 100px
+}
+.other{
+	align: center;
+	margin-top: 100px;
+}
+p{
+	background-color: Blue;
+	padding:10xp;
+    boder:10xp solid black;	
+}
 
-class buyer extends CI_Controller {
+Body{
 
-    public function __construct(){
- 
-        parent::__construct();
-  			$this->load->helper(array('form', 'url'));
-  	 		$this->load->model('buyer_model');
-        $this->load->library('session');
- 
 }
-    
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('table');
-	}
-    
-        public function buyer_view(){
- 
-$this->load->view("buyer.php");
- 
-}    
-    
- public function buy(){
-   
- $buy=array(
-     'username'=>$this->input->post('username'),
-      'status'=>'Pending',
-      'landid'=>$this->input->post('landid'));
-      $landid_check=$this->buyer_model->landid_check($buy['landid']);
- 
-    if($landid_check){
-  $this->buyer_model->buy($buy);
-    $this->session->set_flashdata('success_msg', 'Request Successfull! Now Pending');
-redirect('buyer/buyer_view');
-        
+.box{
+    width: 320px;
+    height: 420px;
+    background: rgba(0,0,0,0.5);
+    color: #fff;
+    top: 60%;
+    left: 40%;
+    position: absolute;
+    transform: translate(-50%,_50%);
+    box-sizing:border-box;   
+    padding: 70px 30px;
 }
-else{
-$this->session->set_flashdata('error_msg', 'Error occured,Try again.');
- redirect('user/buyer_view');
+.box p{
+    margin: 0;
+    padding: 0;
+    font-weight: bold;
+}
+.box input{
+    width: 100%;
+    margin-bottom: 20px;    
+}
+.box input[type="text"]
+{
+    border: none;
+    border-bottom: 1px solid #fff;
+    background: transparent;
+    outline: none;
+    height: 40px;
+    color: #fff;
+    font-size: 16px;
+}
+.box input[type="submit"]
+{
+    border: none;
+    outline: none;
+    height: 40px;
+    background: #1c8adb;
+    color: #fff;
+    font-size: 18px;
+    border-radius: 20px;
+}
+.box input[type="submit"]:hover
+{
+    cursor: pointer;
+    background: #39dc79;
+    color: #000;
+}
+.box a{
+    text-decoration: none;
+    font-size: 14px;
+    color: #fff;
+}
+.box a:hover{
+    color:#39dc79;
+}   
+    body{
+   background-color: #ADD8E6;
+} 
     
-}
+  </style>
+	
+<link href="<?php echo base_url('assets/css/table.css')?>" rel="stylesheet">
+
+</head>
+<body>
+
+<h1> LAND DATA </h1>
+
+<div class= "maindiv">
+
+
+          <?php
+                  $error_msg=$this->session->flashdata('success_msg');
+                  if($error_msg){
+                    echo $error_msg;
+                  }
+                   ?>
     
-}
-}
+ </div>
+    <div class="box">
+            <h1> BUY </h1>
+            <form method="post" action="<?php echo base_url('buyer/buy'); ?>">
+                <p>Username</p>
+                <input type="text" name="username" placeholder = "Enter Username">
+                <p>Land ID</p>
+                <input type="text" name="landid" placeholder = "Enter Land-ID ">
+                <input type="submit" name= "submit" value="BUY">
+            </form>
+    </div>
+
+    
+    
+    </body>
+</html>
+    
+
+    
--- seller.php
+++ seller.php
@@ -1,35 +1,96 @@
-<?php
- 
-class seller extends CI_Controller {
- 
-public function __construct(){
- 
-        parent::__construct();
-  			$this->load->helper(array('form', 'url'));
-  	 		$this->load->model('seller_model');
-        $this->load->library('session');
- 
-}
- 
-            public function seller_view(){
- 
-$this->load->view("seller.php");
- 
-}
 
-public function add(){
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Book venues online</title>
+<style>
+        body{
+   background-color: #ADD8E6;
+} 
+    </style>
+    <script language="javascript"  type="text/javascript">
+        function validate_form(){
+        var location = document.uploadForm.location.value
+        var price = document.uploadForm.price.value
+        var acreage = document.uploadForm.acreage.value
+
+        if (location==null || location==""){
+          alert("First name must be filled")
+          return false;
+
+        }else if(bookingFee==null ||  bookingFee==""){
+          alert("Last name must be filled")
+          return false;
+
+        }else if (guestCapacity==null || guestCapacity==""){
+          alert("Email must be filled")
+          return false;
+
+        }else {
+          return true;
+
+        }
+      }
+    </script>
+
+</head>
+
+<body>
+  <div align="right">
+    <?php
+
+              echo "Logged in as tobi1 ";
  
-      $land=array(
-      'location'=>$this->input->post('location'),
-      'price'=>$this->input->post('price'),
-      'acreage'=>$this->input->post('acreage')
-      
-        );
-        
-  $this->seller_model->add($land);
-  $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
-  redirect('seller/seller_view');
- 
- 
-}
-}
+    ?>
+  </div>
+
+<div class="container">
+  <div>
+   <nav>
+
+
+    </nav>
+  </div>
+</div>
+
+  <div align="center">
+  <h1>Add Land</h1><br>
+
+<form name="uploadForm" action="<?php echo base_url('seller/add'); ?>" method="POST" enctype="multipart/form-data">
+  <table>
+  <tr>
+	   <td>Location:</td>
+     <td><input type="text" name="location" placeholder="Location (e.g. Naivasha)" /></td>
+
+  </tr>
+
+  <tr>
+     <td>Price (Ksh)</td>
+     <td><input type="text" name="price" placeholder="Price (Ksh)" /></td>
+
+  </tr>
+
+  <tr>
+     <td>Acreage</td>
+     <td><input type="text" name="acreage" placeholder="Acreage" /></td>
+
+
+  </tr>
+
+  <tr>
+     <td>Image</td>
+     <td><input type="file" name="foto"></td><br>
+
+  </tr>
+
+</table>
+
+<button type="submit">Add Land</button>
+</form>
+
+  </div>
+
+<a href="<?php echo base_url('user/login_view');  ?>"><button type="button">Back</button></a>
+</body>
+</html>
